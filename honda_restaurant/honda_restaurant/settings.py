706b0bbe1d4b2b37630028cf364bbbbf2b2e324c
Lucas Honda <eliashondalucas@hotmail.com>
2021-03-25 04:05:45 -0300

    set the local host db

--- honda_restaurant/honda_restaurant/settings.py
+++ honda_restaurant/honda_restaurant/settings.py
@@ -82,13 +82,9 @@ WSGI_APPLICATION = 'honda_restaurant.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "db",
-        "PORT": 5432,
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
